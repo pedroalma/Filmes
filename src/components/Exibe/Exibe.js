9c60e7b01c8172add5034cc1de3a4c1533f3506f
pedroalma <pedroalma554@gmail.com>
2023-10-16 11:24:57 -0300

    alteracao2

--- src/components/Exibe/Exibe.js
+++ src/components/Exibe/Exibe.js
@@ -2,7 +2,10 @@ import './Exibe.css';
 function Exibe({img , nome , ano ,}){
     return(
         <>
+        <figure className='conteiner'>
          <img src={img} alt=""/>
+         <figcaption>destaques</figcaption>
+         </figure>
          <h5>{nome}</h5>
          <h5>{ano}</h5>
          </>
